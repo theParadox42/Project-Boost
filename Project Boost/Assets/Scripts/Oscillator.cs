36bbb29eaa75eb55bf745dcb390bd256cb96860c
Paradox Programming <nathaniel.fargo@gmail.com>
2019-06-11 20:59:56 -0600

    afds

--- Project Boost/Assets/Scripts/Oscillator.cs
+++ Project Boost/Assets/Scripts/Oscillator.cs
@@ -2,12 +2,14 @@
 
 public class Oscillator : MonoBehaviour
 {
-
     [SerializeField] Vector3 movementVector = new Vector3(10f, 0f, 0f);
     [SerializeField] float period = 2f;
+    //[SerializeField] [Range(0, 360)] float rotated = 0f;
+    [SerializeField] bool useCos = false;
     float movementFactor = 0; // 0 = start, 1 = end
 
     Vector3 startingPos;
+    Vector3 previousOffset = new Vector3(0, 0, 0);
 
     // Start is called before the first frame update
     void Start()
@@ -21,9 +23,30 @@ public class Oscillator : MonoBehaviour
         if (period <= Mathf.Epsilon) period = 1f;
         float cycles = Time.time / period; // grows
         const float tau = Mathf.PI * 2f; // A number
-        float rawSinWave = Mathf.Sin(cycles * tau);
+        float rawSinWave;
+        if (useCos)
+        {
+            rawSinWave = Mathf.Sin(cycles * tau);
+        } else
+        {
+            rawSinWave = Mathf.Cos(cycles * tau);
+        }
         movementFactor = rawSinWave / 2f + 0.5f;
         Vector3 offset = movementVector * movementFactor;
+        if (offset.x <= Mathf.Epsilon)
+        {
+            offset.x = transform.position.x - startingPos.x;
+        }
+        if(offset.y <= Mathf.Epsilon)
+        {
+            offset.y = startingPos.y - transform.position.y;
+        }
+        if(offset.z <= Mathf.Epsilon)
+        {
+            offset.z = startingPos.z - transform.position.z;
+        }
         transform.position = startingPos + offset;
+
+        previousOffset = offset;
     }
 }
