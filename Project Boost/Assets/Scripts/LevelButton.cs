fdd49284ff35aa594da809f10e02376e0fc44fc1
Paradox Programming <nathaniel.fargo@gmail.com>
2019-06-19 12:41:13 -0600

    Finished basic menu

---
+++ Project Boost/Assets/Scripts/LevelButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LevelButton : MonoBehaviour
+{
+    public int level;
+    private void Start()
+    {
+        
+    }
+    public void SetText(string text)
+    {
+        GetComponentInChildren<TextMeshProUGUI>().text = text;
+    }
+}
--- Project Boost/Assets/Scripts/LevelButtonLoader.cs
+++ Project Boost/Assets/Scripts/LevelButtonLoader.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -13,28 +11,36 @@ public class LevelButtonLoader : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int sceneCount = SceneManager.sceneCountInBuildSettings;
-        bool isLeft = false;
-        float by = 50f;
+        int levelCount = SceneManager.sceneCountInBuildSettings - 3;
+        bool isLeft = true;
+        float by = 50f + 19.75f;
         int level = 0;
-        for(var i = 0; i < sceneCount; i++)
+        RectTransform parentRectTransform = transform.parent.GetComponent<RectTransform>();
+        for (var i = 0; i < levelCount; i++)
         {
-            print(sceneCount);
-            Button newLevelButton = Instantiate(isLeft ? LevelButtonLeft : LevelButtonRight, transform.parent.GetComponent<RectTransform>()) as Button;
-            var rectTransform = newLevelButton.GetComponent<RectTransform>();
-            rectTransform.position = new Vector3(rectTransform.position.x, by, 0f);
-            void loadMyScene()
-            {
-                sceneLoader.LoadSpecificScene(level + 1);
-            }
-            newLevelButton.onClick.AddListener(loadMyScene);
-            level++;
-            isLeft = !isLeft;
-            if (!isLeft)
-            {
-                by -= 100f;
-            }
+            CreateButton(ref isLeft, ref by, ref level, parentRectTransform);
         }
     }
 
+    private void CreateButton(ref bool isLeft, ref float by, ref int level, RectTransform parentRectTransform)
+    {
+        Button newLevelButton = Instantiate(isLeft ? LevelButtonLeft : LevelButtonRight, parentRectTransform) as Button;
+        var rectTransform = newLevelButton.GetComponent<RectTransform>();
+        rectTransform.position = new Vector3(rectTransform.position.x, by, 0f);
+        void loadMyScene(Button self)
+        {
+            int loadLevel = self.GetComponent<LevelButton>().level;
+            sceneLoader.LoadSpecificScene(loadLevel);
+        }
+        LevelButton buttonComponent = newLevelButton.GetComponent<LevelButton>();
+        buttonComponent.level = level + 1;
+        buttonComponent.SetText("Level " + (level + 1).ToString());
+        newLevelButton.onClick.AddListener(() => loadMyScene(newLevelButton));
+        level++;
+        isLeft = !isLeft;
+        if (isLeft)
+        {
+            by -= 100f;
+        }
+    }
 }
